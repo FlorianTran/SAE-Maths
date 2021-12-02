ccdf19a9d3f2d83406e2bac9d7fbd3b9b533c08c
deidasai <freret.tom@gmail.com>
2021-12-02 11:11:26 +0100

    Update main.py
    question 1

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-print("Hello world !")
+### RAS ###
 
-print("test")
+# Q. 1.1
 
-print("tom est une salope")
+# Renvoie un tableau de tous les nombres premiers entre 0 et n
 
-print("test2")
+
+def list_prim(n):
+    result = []
+    if n <= 1:
+        return result
+    for num in range(2, n+1):
+        if is_prime(num):
+            result.append(num)
+    return result
+
+# Renvoie si nombre n est enier ou pas
+
+
+def is_prime(n):
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+# Q. 1.2
