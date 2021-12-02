fcec5aa67c8d4a54f7d77064cd96e39d632b3a0d
FlorianTran <florian.tranpro@gmail.com>
2021-12-02 11:24:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,25 @@ def list_prim(n):
     if n <= 1:
         return result
     for num in range(2, n+1):
-        if is_prime(num):
+        if is_prim(num):
             result.append(num)
     return result
 
 # Renvoie si nombre n est enier ou pas
-def is_prime(n):
+def is_prim(n):
     for i in range(2, int(math.sqrt(n)) + 1):
         if n % i == 0:
             return False
     return True
 
+""" def primmaisvrmrapide(n):
+    out = list()
+    sieve = [True] * (n+1)
+    for p in range(2, n+1):
+        if (sieve[p] and sieve[p] % 2 == 1):
+            out.append(p)
+            for i in range(p, n+1, p):
+                sieve[i] = False
+    return out """
+
 # Q. 1.2
-print("test")
