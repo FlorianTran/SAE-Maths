95f083fe1a8d6bf8c5ee5516b368cfd518a0f327
FlorianTran <florian.tranpro@gmail.com>
2021-12-13 09:01:18 +0100

    Update SAE_S1_02_RSA.py

--- SAE_S1_02_RSA.py
+++ SAE_S1_02_RSA.py
@@ -182,3 +182,6 @@ def reconvert_msg(decrypted_msg_ascii):
     return decrypted_msg
 
 # pb avec had qui renvois un nul en plus en gros c'est a cause de ascii ou 000 est NUl un caractère de controle donc jsp
+""" 
+n,pub,priv=key_creation()
+msg = reconvert_msg(decryption_msg(n, priv, encryption_msg(n, pub, convert_msg("")))) """
